11afaa36ff447715cd7d546e5f0ae448a844417d
jaonai <79089805+patchanonpinyosuwan@users.noreply.github.com>
2024-01-12 16:49:23 +0700

    Add files via upload

---
+++ sketch_jan12a_copy_20240112164303_copy_20240112164848.ino
@@ -0,0 +1,48 @@
+#include <TridentTD_LineNotify.h>
+
+
+#define SSID        "PP"      //ใส่ ชื่อ Wifi ที่จะเชื่อมต่อ
+#define PASSWORD    "Jaonai133279"     //ใส่ รหัส Wifi
+#define LINE_TOKEN  "k55b0fsXMEEDLLcyelRQThTJhUsmaMM3Nrup7Wo2pJu"   //ใส่ รหัส TOKEN ที่ได้มาจากข้างบน
+
+int ledPin = D0;
+int GAS = A0;//ประกาศตัวแปร ให้ GAS
+int val = 0;
+void setup() {
+  pinMode(ledPin, OUTPUT);  // sets the pin as output
+
+  Serial.begin(9200);     // Corrected the syntax for Serial.begin
+  Serial.println(LINE.getVersion());  // Corrected the typo in Serial.println
+
+  WiFi.begin(SSID, PASSWORD);
+  Serial.print("WiFi connecting to ");
+  Serial.print(SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(400);
+  }
+  Serial.println("\nWiFi connected\nIP: ");
+  Serial.println(WiFi.localIP());
+
+  // Set Line Token
+  LINE.setToken(LINE_TOKEN);
+}
+
+void loop() {
+  val = analogRead(GAS);  //อ่านค่าสัญญาณ analog 
+  Serial.print("val = "); // พิมพ์ข้อมความส่งเข้าคอมพิวเตอร์ "val = "
+  Serial.println(val); // พิมพ์ค่าของตัวแปร val
+  if (val > 500) { // สามารถกำหนดปรับค่าได้ตามสถานที่ต่างๆ
+    digitalWrite(ledPin,HIGH); // สั่งให้ LED กระพริบ
+    delay(500);
+    digitalWrite(ledPin,LOW);
+    delay(500);
+    LINE.notify("มีแก๊ส");
+  }
+  else{
+    digitalWrite(ledPin, LOW); // สั่งให้ LED ดับ
+    return;
+  } 
+  delay(100);
+  return;
+}
